991432299994abb67f6ea4a2efecf2bf471aae76
Luis Zubia <soyzubia@gmail.com>
2023-03-30 21:55:06 -0700

    Add Routes for root, categories and error paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import BookSpot from './routes/BookSpot';
+import Home from './routes/Home';
+import Categories from './routes/Categories';
+import NotFound from './routes/NotFound';
 import './assets/styles/App.css';
 
 const App = () => (
-  <h1>Welcome to BookSpot</h1>
+  <main>
+    <Routes>
+      <Route element={<BookSpot />}>
+        <Route index element={<Home />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </main>
 );
 
 export default App;

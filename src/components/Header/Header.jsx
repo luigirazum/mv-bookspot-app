405e9cd4dac604f8c5b9429e99e4d266dd78df32
Luis Zubia <soyzubia@gmail.com>
2023-04-01 19:58:22 -0700

    Add Header component, it shows the AppTitle, NavBar and Avatar components at the top of the App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import AppTitle from '../AppTitle/AppTitle';
+import NavBar from '../NavBar/NavBar';
+import Avatar from '../Avatar/Avatar';
+
+const Header = () => (
+  <header>
+    <AppTitle />
+    <NavBar />
+    <Avatar />
+  </header>
+);
+
+export default Header;

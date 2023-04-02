8fa755262a9eaa9f8d8645140cbea023ec3f4d12
Luis Zubia <soyzubia@gmail.com>
2023-04-01 19:49:51 -0700

    Wrap the whole App into a div element for styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import BookSpot from './routes/BookSpot';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
+import Profile from './routes/Profile';
 import NotFound from './routes/NotFound';
 import './assets/styles/App.css';
 
 const App = () => (
-  <main>
+  <div className="App">
     <Routes>
       <Route element={<BookSpot />}>
         <Route index element={<Home />} />
         <Route path="categories" element={<Categories />} />
+        <Route path="profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-  </main>
+  </div>
 );
 
 export default App;

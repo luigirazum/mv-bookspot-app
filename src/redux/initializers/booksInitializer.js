9cb0074280b3fbc6a9edd6a352b8c55f6941af9e
Luis Zubia <soyzubia@gmail.com>
2023-04-05 22:46:15 -0700

    Rename item_id by id

--- src/redux/initializers/booksInitializer.js
+++ src/redux/initializers/booksInitializer.js
@@ -2,19 +2,19 @@ import { v4 as uuidv4 } from 'uuid';
 
 const booksInitializer = [
   {
-    item_id: uuidv4(),
+    id: uuidv4(),
     title: 'The Great Gatsby',
     author: 'John Smith',
     category: 'Fiction',
   },
   {
-    item_id: uuidv4(),
+    id: uuidv4(),
     title: 'Anna Karenina',
     author: 'Leo Tolstoy',
     category: 'Fiction',
   },
   {
-    item_id: uuidv4(),
+    id: uuidv4(),
     title: 'The Selfish Gene',
     author: 'Richard Dawkins',
     category: 'Nonfiction',

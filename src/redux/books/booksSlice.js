a039a8e0be57b4c2d171a8d22d6f3f1d2c7f9b7f
Luis Zubia <soyzubia@gmail.com>
2023-04-07 16:39:21 -0700

    Add fetchBooks asyncThunk && Create fetchBooks lifecycle states reducers for pending, fullfill and, rejected && export fetchBooks action

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,12 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
-import booksInitializer from '../initializers/booksInitializer';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import apiRequest from '../api/apiConfig';
 
 const selectLibrary = (store) => store.books;
 
+/*-------------------------------------------------
+ * Action Type DEFINITIONS
+ *-------------------------------------------------*/
+const FETCH_BOOKS = 'books/fetchBooks';
+
 const initialState = {
-  library: [...booksInitializer],
+  library: [],
   isLoading: false,
 };
+
+const fetchBooks = createAsyncThunk(
+  FETCH_BOOKS,
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios
+        .request(apiRequest('GET'));
+      return data;
+    } catch (error) {
+      return thunkAPI
+        .rejectWithValue('We are sorry to tell you that something went wrong.');
+    }
+  },
+);
 
 const booksSlice = createSlice({
   name: 'books',
@@ -32,6 +52,38 @@ const booksSlice = createSlice({
         library: newLibrary,
       });
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending,
+        (state) => ({
+          ...state,
+          isLoading: true,
+        }))
+
+      .addCase(fetchBooks.fulfilled,
+        (state, { payload: data }) => {
+          const fetchedLibrary = Object.entries(data)
+            .map((b) => (
+              {
+                id: b[0],
+                ...data[b[0]][0],
+              }
+            ));
+
+          return ({
+            ...state,
+            library: fetchedLibrary,
+            isLoading: false,
+          });
+        })
+
+      .addCase(fetchBooks.rejected,
+        (state, { payload }) => ({
+          ...state,
+          isLoading: false,
+          error: payload,
+        }));
   },
 });
 

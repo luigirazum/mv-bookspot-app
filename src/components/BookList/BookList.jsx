2bc4160563182e4bd8561d0486f874f176cbc55f
Luis Zubia <soyzubia@gmail.com>
2023-04-07 19:39:54 -0700

    Fetch books from API after rendering the BookList component && Add conditional render to show Loading message

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,9 +1,17 @@
-import { useSelector } from 'react-redux';
-import { selectLibrary } from '../../redux/books/booksSlice';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBooks, selectLibrary, selectIsLoading } from '../../redux/books/booksSlice';
 import Book from '../Book/Book';
 
 const BookList = () => {
-  const { library } = useSelector(selectLibrary);
+  const library = useSelector(selectLibrary);
+  const isLoading = useSelector(selectIsLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   const booksList = library
     .map((book) => (
@@ -13,26 +21,44 @@ const BookList = () => {
       />
     ));
 
+  const showBooksList = () => {
+    if (booksList.length) {
+      return booksList;
+    }
+
+    return (
+      <aside className="booksAvailable">
+        <h2>books available</h2>
+        <p>Right now there are no books.</p>
+        <div className="suggestion">
+          <p>
+            We suggest you to add some books with the
+            {' '}
+            <strong>add new book</strong>
+            {' '}
+            form below.
+          </p>
+        </div>
+      </aside>
+    );
+  };
+
   return (
     <section className="bookList">
       {
-      booksList.length
-        ? booksList
-        : (
+        isLoading ? (
           <aside className="booksAvailable">
             <h2>books available</h2>
-            <p>Right now there are no books.</p>
-            <div className="suggestion">
-              <p>
-                We suggest you to add some books with the
-                {' '}
-                <strong>add new book</strong>
-                {' '}
-                form below.
-              </p>
-            </div>
+            <p
+              className="loading"
+              style={{
+                animationName: 'loading',
+              }}
+            >
+              Loading...
+            </p>
           </aside>
-        )
+        ) : showBooksList()
       }
     </section>
   );

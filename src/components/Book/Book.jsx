5cef46c35f26fb8dd0185a18a8e1c1b7f09183b2
Luis Zubia <soyzubia@gmail.com>
2023-04-09 20:08:04 -0700

    Add inline styles for Book for playing animation

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -9,10 +9,6 @@ const Book = ({ bookItem: { id } }) => {
     title, author, category, chapterName, percent,
   } = book;
 
-  const bpRingStyle = {
-    backgroundImage: `conic-gradient(var(--color-blue-x) ${percent}%, var(--progress-color) 0)`,
-  };
-
   return (
     <article className="book">
       <section className="bookInfo">
@@ -30,12 +26,44 @@ const Book = ({ bookItem: { id } }) => {
         <ActionButtons data={id} />
       </section>
       <section className="bookProgress">
-        <div className="bpIconContainer">
+        <div className="bpSpinnerIcon">
           <div
-            className="bpIcon"
-            style={bpRingStyle}
+            className="bpSpinner"
+            style={{
+              '--spin-progress': percent,
+              '--spin-percent': 'calc(var(--spin-progress) * 360 / 100 * 1deg)',
+              animationName: `spin-${id}`,
+              animationDuration: '3s',
+              animationFillMode: 'forwards',
+              animationTimingFunction: 'ease-out',
+            }}
           >
-            <div className="bpIconInner" />
+            <style>
+              {`@keyframes spin-${id} {
+                0% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0), var(--color-gray-c4) 0deg); }
+                5% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.05), var(--color-gray-c4) 0deg); }
+                10% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.10), var(--color-gray-c4) 0deg); }
+                15% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.15), var(--color-gray-c4) 0deg); }
+                20% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.20), var(--color-gray-c4) 0deg); }
+                25% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.25), var(--color-gray-c4) 0deg); }
+                30% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.30), var(--color-gray-c4) 0deg); }
+                35% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.35), var(--color-gray-c4) 0deg); }
+                40% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.40), var(--color-gray-c4) 0deg); }
+                45% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.45), var(--color-gray-c4) 0deg); }
+                50% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.50), var(--color-gray-c4) 0deg); }
+                55% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.55), var(--color-gray-c4) 0deg); }
+                60% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.60), var(--color-gray-c4) 0deg); }
+                65% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.65), var(--color-gray-c4) 0deg); }
+                70% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.70), var(--color-gray-c4) 0deg); }
+                75% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.75), var(--color-gray-c4) 0deg); }
+                80% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.80), var(--color-gray-c4) 0deg); }
+                85% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.85), var(--color-gray-c4) 0deg); }
+                90% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.90), var(--color-gray-c4) 0deg); }
+                95% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 0.95), var(--color-gray-c4) 0deg); }
+                100% { background-image: conic-gradient(var(--color-blue-x) calc(var(--spin-percent) * 1), var(--color-gray-c4) 0deg); }
+              }
+              `}
+            </style>
           </div>
         </div>
         <div className="bpData">
